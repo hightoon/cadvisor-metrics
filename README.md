e8125220ba95f9d1db3d1b4331f7329707aea7d3
uphoff <ben@catalyze.io>
2015-04-14 12:18:54 -0500

    First commit after splitting out open source components from existing private repo.

---
+++ README.md
@@ -0,0 +1,231 @@
+# cAdvisor Metrics
+
+This repo addresses the problem of collecting [Docker](https://github.com/docker/docker) container metrics from [cadvisor](https://github.com/google/cadvisor) at scale.
+While cadvisor has a nice [InfluxDB](https://github.com/influxdb/influxdb) integration, this project can be viewed as an alternative, or additional, means of storing cadvisor metrics. 
+
+At Catalyze, our [Platform as a Service](https://catalyze.io/paas) uses Docker containers to host customer applications
+and associated infrastructure (databases, logging, etc). A single customer environment will span multiple hosts and therefore
+we need a mechanism for centralized metrics collection and aggregation.
+
+The approach used here is to collect metrics at the container level using cadvisor and then send them back to a centralized collection point.
+Currently cadvisor returns the last minute's worth of metrics by default, in 1 second intervals. The sender process
+runs every minute (via cron) and then rolls up the 1 second intervals into a 1 minute interval. The collector will put the result
+into [Redis](https://github.com/antirez/redis) and trim off the oldest entry in order to maintain a sliding window.
+
+The metrics stored in Redis are transient in that only the last 1440 entries (1 day, configurable) are stored. 
+Entries should be rolled off into some other application or collection tool for long term storage and analysis.
+
+## Collector
+
+The collector is a Falcon API endpoint, currently a single POST route that accepts metrics from senders running on
+any number of hosts. The collector receives Docket container metrics from hosts running the sender (see below) and 
+stores them in Redis. 
+
+The following section explains how to run the container via Docker.
+If you are not interested in running the collector this way you can run it via python. 
+See [the source](collector/collector.py) for instructions.
+
+### Running with Docker:
+
+Start by building the collector image (these steps assume the repo is cloned and is the working directory):
+
+    cd collector/
+    sudo docker build -t collector .
+
+To run the collector, pass in the port to use in COLLECTOR_PORT and expose that port. 
+You will also need to provide the Redis IP and port (defaults to `redis.local` and `6379` respectively). 
+
+    sudo docker run \
+      -e 'COLLECTOR_REDIS_HOST=192.168.222.5' \
+      -e 'COLLECTOR_REDIS_PORT=6379' \
+      -e 'COLLECTOR_PORT=8787' \
+      --restart on-failure:5 \
+      --name collector \
+      -p 8787:8787 -d -t collector
+      
+
+## Sender
+
+The sender is a Docker container that runs a python script (sender.py) every minute.
+It polls cadvisor and forwards the results to the collector. 
+It should be deployed on every host where container metrics are to be collected. 
+
+### Running cadvisor
+
+The sender polls cadvisor every minute via cadvisor's REST API. 
+In these instructions we expose cadvisor on port 8989.
+The currently supported cadvisor version is 0.9.0.
+See the [cadvisor repo](https://github.com/google/cadvisor) for more information.
+
+    sudo docker run \
+      --volume=/:/rootfs:ro \
+      --volume=/var/run:/var/run:rw \
+      --volume=/sys:/sys:ro \
+      --volume=/var/lib/docker/:/var/lib/docker:ro \
+      --publish=8989:8080 \
+      --detach=true \
+      --restart on-failure:5 \
+      --name=cadvisor \
+      google/cadvisor:0.9.0
+
+### Running the sender
+
+The sender needs to know where to get metrics from cadvisor and where to send them (the collector's IP and port).
+
+First build the image:
+
+    cd ../sender/
+    sudo docker build -t sender .
+
+Start the container: 
+
+    sudo docker run \
+      -e 'COLLECTOR_URL=http://192.168.222.5:8787/cadvisor/metrics/' \
+      -e 'CADVISOR_URL=http://192.168.222.6:8989/api/v1.2' \
+      --restart on-failure:5 \
+      --name sender \
+      -d sender
+
+## Accessing Metrics
+
+By default the collector will keep the last 1440 minutes of data (one days' worth) in Redis.
+Each container will have 1440 1-minute metrics.
+The results are stored as Redis [lists](http://redis.io/commands#list) with keys of the form `stat:<container-name>`.
+Below are some examples of how to access various metrics and data items via `redis-cli`.
+
+Get a list of containers:
+
+    smembers names
+
+Get container metadata (e.g. IP address):
+
+    get name:<container-name>
+
+Determine the number of entries for a container:
+
+    llen stats:<container-name>
+
+Retrieve the 1st entry:
+
+    lrange stats:<container-name> 0 0
+
+Retrieve all entries:
+
+    lrange stats:<container-name> 0 -1
+
+Get the machine info for an IP (stores the /machine route data from cadvisor):
+
+    get ip:<host-ip>
+    
+## Metrics JSON Format
+
+Data is stored as JSON in Redis. The top level keys are as follows:
+ 
+* **name**: the name of the container, a string
+* **ts**: the starting minute of the entry in epoch time (seconds)
+* **network**: stats for network bytes and packets in an out of the container
+* **diskio**: disk I/O stats for the container
+* **memory**: memory usage in KB
+* **cpu**: cpu usage from the start of the minute to the end
+
+### Network data:
+* Each **network** section contains **tx_bytes**, **rx_bytes**, **tx_packets** and **rx_packets**
+* Each sub-section contains **ave** (average), **min** (minimum) and **max** (maximum) values
+
+### Disk I/O data:
+* Each **diskio** section contains **read**, **write**, **async** and **sync**
+* The I/O values are computed as the delta of the value from the beginning of the minute to the end of the minute
+* The values for **read** and **write** are in KB
+* The values for **sync** and **async** are in number of operations
+* The total bytes is equal to the sum of reads and writes
+* The total number of I/O operations is equal to the sum of async and sync
+
+### Memory data:
+* The **memory** section contains **ave** (average), **min** (minimum) and **max** (maximum) values
+* All values are in KB
+
+### CPU data:
+* The **usage** value is the delta of cumulative CPU usage from the beginning of the minute to the end of the minute. 
+* The **load** section seems to always report 0. This is considered an outstanding bug to be addressed in a future release. 
+
+Here is an example entry:
+ 
+```
+{
+  "name": "471c2fd8-674c-4397-a660-e80338e01269",
+  "ts": 1427762581,
+  "network": {
+    "tx_bytes": {
+      "ave": 235189226,
+      "min": 235174806,
+      "max": 235203379
+    },
+    "rx_packets": {
+      "ave": 899691,
+      "min": 899603,
+      "max": 899776
+    },
+    "tx_packets": {
+      "ave": 1214692,
+      "min": 1214571,
+      "max": 1214811
+    },
+    "rx_bytes": {
+      "ave": 2885699730,
+      "min": 2885390818,
+      "max": 2886008373
+    }
+  },
+  "diskio": {
+    "read": 0,
+    "async": 0,
+    "write": 0,
+    "sync": 0
+  },
+  "memory": {
+    "ave": 252555,
+    "min": 252176,
+    "max": 252868
+  },
+  "cpu": {
+    "usage": 216588252,
+    "load": {
+      "ave": 0,
+      "min": 0,
+      "max": 0
+    }
+  }
+}
+```
+
+## Post-processing
+
+We run a number of scripts against the collected metrics to monitor customer applications and gain insights into potential load problems.
+For this initial release we are providing an [example script](scripts/stats_by_ip.py) that aggregates metrics on a per-IP basis. 
+This script is best run periodically via cron and then analyzed with something like [pandas](http://pandas.pydata.org/).
+
+Run the script after adjusting the Redis host/port as needed and record the output:
+
+    python stats_by_ip.py > stats.csv
+
+Here is some example code suitable for an [ipython notebook](http://ipython.org/notebook.html) for plotting memory usage by IP: 
+
+```
+%matplotlib inline
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+pd.set_option('max_columns', 50)
+
+
+stats = pd.read_csv('stats.csv')
+by_ip = stats.groupby('ip')
+by_ip['memory'].mean().plot(kind='bar')
+```
+
+# Contributing
+
+If you find something wrong with our code please submit a GitHub issue or, better yet, submit a pull request. 
+For other inquiries please email support@catalyze.io.   
+
---
+++ sender/sender.py
@@ -0,0 +1,258 @@
+from __future__ import print_function
+
+"""
+This script is intended to be run via cron every minute. 
+It gathers the last minute of cadvisor stats, rolls them up and then
+uploads stats to the collector endpoint. 
+
+For this script to run, cadvisor (https://github.com/google/cadvisor) must be running
+along with the collector (see collector/collector.py).
+
+Environment variable examples:
+
+    # The URL for the cadvisor API
+    CADVISOR_URL=http://192.168.222.5:8989/api/v1.2
+
+    # The URL for the collector endpoint
+    COLLECTOR_URL=http://192.168.222.5:8787/cadvisor/metrics
+
+Running:
+
+    python sender.py
+
+"""
+import time
+import json
+import requests
+import uuid
+import dateutil.parser
+import os
+
+# Determine the collector URL. The default collector.local address is used to make running via docker easier.
+endpoint = os.getenv('COLLECTOR_URL', 'http://collector.local:8787/cadvisor/metrics/')
+
+# Determine the cadvisor URL. The default cadvisor.local address is used to make running via docker easier.
+# Note that port 8989 is being used below, which is not the standard port given in cadvisor's documentation examples.
+cadvisor_base = os.getenv('CADVISOR_URL', 'http://cadvisor.local:8989/api/v1.2')
+
+# The following functions are examples of different approaches for detemining which containers to report stats on
+
+def match_all_but_cadvisor(name):
+    """
+    Match on anything that isn't cadvisor. 
+    """
+    if 'cadvisor' in value['aliases']:
+        return False
+    return True
+
+def match_all(name):
+    """
+    Match on all containers, including cadvisor.
+    """
+    return True
+
+def match_on_uuid(name):
+    """
+    Match on any container that has a UUID as its name.
+
+    This is used internally at Catalyze, as all of our customer containers have a UUID as their container name.
+    """
+
+    if 'cadvisor' in value['aliases']:
+        return False # Ignore cadvisor
+
+    try:
+        # Try to parse as a UUID and match if it works
+        uuid.UUID(name, version=1)
+        return True
+    except ValueError:
+        # Not a UUID so not a container to match on
+        return False
+
+
+# Set the function to use when determining which containers to report on
+# Set the MATCH_TYPE environment variable to UUID or NO_CADVISOR to change the function to use
+match_type_name = os.getenv('MATCH_TYPE', 'ALL')
+match_container_name = match_all # Default
+if match_type_name == 'UUID':
+    match_container_name = match_on_uuid
+elif match_type_name == 'NO_CADVISOR':
+    match_container_name = match_all_but_cadvisor
+
+def total_min_max(stat, s_total, s_min, s_max):
+    """
+    Given a value (stat), add it to the total and check if it is the new
+    min value or max value compared to s_min and s_max.
+    """
+    result_min = s_min
+    result_max = s_max
+    s_total += stat
+    if s_min == None or stat < s_min:
+        result_min = stat
+    if s_max == None or stat > s_max:
+        result_max = stat
+    return s_total, result_min, result_max
+
+def process_diskio(diskio, field):
+    """
+    Sum up all the disk IO bytes for a given field (Sync, Async, Read, Write).
+
+    Only considering io_service_bytes stats right now (io_serviced is ignored).
+    """
+
+    total = 0
+    print(diskio)
+    io_stats = diskio['io_service_bytes']
+    for entry in io_stats:
+        total += entry['stats'][field]
+
+    return total
+        
+
+# Connect to cadvisor and get the last minute's worth of stats (should be 60 stats per container)
+r = requests.get('%s/docker' % cadvisor_base)
+entries = []
+for key, value in r.json().items():
+
+    # Determine if one of the aliases matches (is something we want to collect metrics for)
+    container_name = None
+    for name in value['aliases']:
+        if match_container_name(name):
+            container_name = name
+            break
+
+    # Skip this if the container didn't match
+    if container_name == None:
+        continue
+
+    # Compute the timestamp, using the first second in this series
+    ts = int(dateutil.parser.parse(value['stats'][0]['timestamp']).strftime('%s'))
+    
+    # Run through all the stat entries for this container
+    stats = value['stats']
+    stats_len = len(stats) # Should always be 60
+
+    # Initialize min/max/total variables for network KB, packets, memory, cpu
+    total_memory = 0
+    min_memory = None
+    max_memory = None
+    total_load = 0
+    min_load = None
+    max_load = None
+    total_tx_bytes = 0
+    min_tx_bytes = None
+    max_tx_bytes = None
+    total_rx_bytes = 0
+    min_rx_bytes = None
+    max_rx_bytes = None
+    total_tx_packets = 0
+    min_tx_packets = None
+    max_tx_packets = None
+    total_rx_packets = 0
+    min_rx_packets = None
+    max_rx_packets = None
+
+    for stat in stats:
+        print(json.dumps(stat))
+
+        # Grab the memory usage stats
+        memory = stat['memory']
+        memory_kb = memory['usage']/1024
+        total_memory, min_memory, max_memory = total_min_max(memory_kb, total_memory, min_memory, max_memory)
+    
+        # Get the CPU stats. The load value is always 0?
+        cpu = stat['cpu']
+        cpu_load = cpu['load_average']
+        total_load, min_load, max_load = total_min_max(cpu_load, total_load, min_load, max_load)
+
+        # Grab the network stats
+        network = stat['network']
+        total_tx_bytes, min_tx_bytes, max_tx_bytes = total_min_max(network['tx_bytes'], total_tx_bytes, min_tx_bytes, max_tx_bytes)
+        total_rx_bytes, min_rx_bytes, max_rx_bytes = total_min_max(network['rx_bytes'], total_rx_bytes, min_rx_bytes, max_rx_bytes)
+        total_tx_packets, min_tx_packets, max_tx_packets = total_min_max(network['tx_packets'], total_tx_packets, min_tx_packets, max_tx_packets)
+        total_rx_packets, min_rx_packets, max_rx_packets = total_min_max(network['rx_packets'], total_rx_packets, min_rx_packets, max_rx_packets)
+        # Not handling drops right now for simplicity
+
+    # Initialize the entry for this container
+    entry = {'name': container_name, 'ts' : ts}
+    entry['cpu'] = {}
+    entry['memory'] = {}
+    entry['network'] = {}
+    entry['diskio'] = {}
+
+    # Compute first/last values of cumulative counters
+    first = stats[0] # First item in this series
+    last = stats[stats_len-1] # Last item in this series
+
+    # Compute CPU usage delta
+    start_cpu_usage = first['cpu']['usage']['total']
+    end_cpu_usage = last['cpu']['usage']['total']
+
+    # Compute Disk IO deltas
+    start_async_bytes = process_diskio(first['diskio'], 'Async')
+    end_async_bytes = process_diskio(last['diskio'], 'Async')
+    start_sync_bytes = process_diskio(first['diskio'], 'Sync')
+    end_sync_bytes = process_diskio(last['diskio'], 'Sync')
+    start_read_bytes = process_diskio(first['diskio'], 'Read')
+    end_read_bytes = process_diskio(last['diskio'], 'Read')
+    start_write_bytes = process_diskio(first['diskio'], 'Write')
+    end_write_bytes = process_diskio(last['diskio'], 'Write')
+
+    # Add CPU stats
+    entry['cpu']['usage'] = end_cpu_usage - start_cpu_usage
+    entry['cpu']['load'] = {}
+    entry['cpu']['load']['ave'] = total_load/stats_len
+    entry['cpu']['load']['min'] = min_load
+    entry['cpu']['load']['max'] = max_load
+
+    # Add memory stats
+    entry['memory']['ave'] = total_memory/stats_len
+    entry['memory']['min'] = min_memory
+    entry['memory']['max'] = max_memory
+
+    # Add network stats
+    entry['network']['tx_bytes'] = {}
+    entry['network']['tx_bytes']['ave'] = total_tx_bytes/stats_len 
+    entry['network']['tx_bytes']['min'] = min_tx_bytes
+    entry['network']['tx_bytes']['max'] = max_tx_bytes
+    entry['network']['rx_bytes'] = {}
+    entry['network']['rx_bytes']['ave'] = total_rx_bytes/stats_len 
+    entry['network']['rx_bytes']['min'] = min_rx_bytes
+    entry['network']['rx_bytes']['max'] = max_rx_bytes
+    entry['network']['tx_packets'] = {}
+    entry['network']['tx_packets']['ave'] = total_tx_packets/stats_len 
+    entry['network']['tx_packets']['min'] = min_tx_packets
+    entry['network']['tx_packets']['max'] = max_tx_packets
+    entry['network']['rx_packets'] = {}
+    entry['network']['rx_packets']['ave'] = total_rx_packets/stats_len 
+    entry['network']['rx_packets']['min'] = min_rx_packets
+    entry['network']['rx_packets']['max'] = max_rx_packets
+    # Note that errors are not being reported, easy enough to add
+
+    # Add disk IO stats
+    # These stats are currently aggregated across all volumes. May not be desirable.
+    entry['diskio']['async'] = end_async_bytes - start_async_bytes
+    entry['diskio']['sync'] = end_sync_bytes - start_sync_bytes
+    entry['diskio']['read'] = end_read_bytes - start_read_bytes
+    entry['diskio']['write'] = end_write_bytes - start_write_bytes
+    # Note that io_serviced stats are not being included here. Easy to add if needed.
+
+    entries.append(entry)
+
+interval = 60 # Number of seconds of data that we are getting back from cadvisor per container
+
+# Create the final result to send to the collector
+stats_result = {}
+stats_result['ts'] = int(time.time()) # Epoch time for when this entry was computed (in seconds)
+stats_result['interval'] = interval # The duration of this stat entry in seconds
+stats_result['stats'] = entries
+
+# Sending along info about the host, as tracked by cadvisor
+# This data can easily be extended to include other sources (/proc, etc)
+r = requests.get('%s/machine' % cadvisor_base)
+stats_result['machine'] = r.json()
+
+# POST the result to the collector
+headers = {'content-type': 'application/json'}
+post_result = requests.post(endpoint, data=json.dumps(stats_result), headers=headers)
+post_result.raise_for_status()
